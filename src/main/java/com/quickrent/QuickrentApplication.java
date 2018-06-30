50b4bfc43a5f69f10e3e3fea4d8eb85acf3117eb
enble du <384509085@qq.com>
2018-06-30 21:48:41 +0800

    template

---
+++ src/main/java/com/quickrent/QuickrentApplication.java
@@ -0,0 +1,12 @@
+package com.quickrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickrentApplication.class, args);
+	}
+}
