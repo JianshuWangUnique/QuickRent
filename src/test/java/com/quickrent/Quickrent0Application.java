eb81bfbb92c8207b2d967788f994c4940129bb54
JianshuWangUnique <39620768+JianshuWangUnique@users.noreply.github.com>
2018-07-09 16:11:23 +0800

    Add files via upload

---
+++ src/test/java/com/quickrent/Quickrent0Application.java
@@ -0,0 +1,12 @@
+package quickRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quickrent0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Quickrent0Application.class, args);
+    }
+}
